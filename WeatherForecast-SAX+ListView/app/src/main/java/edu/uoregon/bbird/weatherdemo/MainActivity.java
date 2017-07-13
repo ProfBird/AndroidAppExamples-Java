159c5135158461e24504b652fb435f241189da23
Brian Bird <profbird@gmail.com>
2017-07-13 10:42:23 -0700

    Added String constants

--- WeatherForecast-SAX+ListView/app/src/main/java/edu/uoregon/bbird/weatherdemo/MainActivity.java
+++ WeatherForecast-SAX+ListView/app/src/main/java/edu/uoregon/bbird/weatherdemo/MainActivity.java
@@ -16,6 +16,8 @@ public class MainActivity extends Activity
 				implements OnItemClickListener {
 
 	private WeatherItems weatherItems;
+    static final String DATE = "date";
+    static final String IMAGE_NAME = "imageName";
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +37,10 @@ public class MainActivity extends Activity
         for (WeatherItem item : weatherItems)
         {
         	HashMap<String, String> map = new HashMap<String, String>();
-        	map.put("date", item.getForecastDateFormatted());
+        	map.put(DATE, item.getForecastDateFormatted());
         	// map.put("lowTemp", getString(R.string.low) + item.getLowTemp());
         	// map.put("highTemp", getString(R.string.high) + item.getHighTemp());
-	    	map.put("imageName",
+	    	map.put(IMAGE_NAME,
         			Integer.toString(getResources().getIdentifier(
         					item.getDescription().toLowerCase().replaceAll("\\s+",""), "drawable", getPackageName() 
         					)));
@@ -52,7 +54,7 @@ public class MainActivity extends Activity
         SimpleAdapter adapter = new SimpleAdapter(this,
         	data,
         	R.layout.listview_items,
-        	new String[]{"imageName", "date"},
+        	new String[]{IMAGE_NAME, DATE},
         	new int[]{R.id.iconImageView,
         			  R.id.dateTextView, 
         			  }
