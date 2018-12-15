b40a61c3e2212d8b537c344dc7558c03870124fa
Brian Bird <birdb@lanecc.edu>
2018-12-15 13:10:18 -0800

    Added week one example

--- GeoDistanceCalculator/app/src/main/java/edu/uoregon/bbird/geodistancecalculator/MainActivity.java
+++ GeoDistanceCalculator/app/src/main/java/edu/uoregon/bbird/geodistancecalculator/MainActivity.java
@@ -1,19 +1,15 @@
 package edu.uoregon.bbird.geodistancecalculator;
 
-// By Brian Bird, July 15, 2016, updated July 19, 2017
-// Demonstrates: 1) getting location using the Fused Location API provided by Google Play Services
+// By Brian Bird, July 15, 2016, updated July 19, 2018 (Refactored to use the FusedLocationProviderClient)
+// Demonstrates: 1) getting location using the Fused Location Provider API provided by Google Play Services
 // 2) using the Android Geocoder to get an address from latitude and longitude
 // 3) using the Android Location class to determine distance between two locaitons
-
-// Android classes
 
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -21,26 +17,19 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-// Google Play Services classes and interfaces
-// Java classes
 
+public class MainActivity extends AppCompatActivity implements OnSuccessListener {
 
-public class MainActivity extends AppCompatActivity
-        implements ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
-
-    GoogleApiClient googleApiClient;
+    FusedLocationProviderClient  locationClient;
     Location myLocation;
     LocationRequest locationRequest;
 
@@ -54,20 +43,14 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        currentLocationTextView = (TextView)findViewById(R.id.currentLocationTextView);
-        distanceTextView = (TextView)findViewById(R.id.distanceTextView);
-        latLonTextView = (TextView)findViewById(R.id.latLonTextView);
-        cityEditText = (EditText)findViewById(R.id.cityEditText);
-        stateEditText = (EditText)findViewById(R.id.stateEditText);
+        currentLocationTextView = (TextView) findViewById(R.id.currentLocationTextView);
+        distanceTextView = (TextView) findViewById(R.id.distanceTextView);
+        latLonTextView = (TextView) findViewById(R.id.latLonTextView);
+        cityEditText = (EditText) findViewById(R.id.cityEditText);
+        stateEditText = (EditText) findViewById(R.id.stateEditText);
 
-        // Get the Location API and register the callbacks that it uses
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(LocationServices.API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-
-        // This object will be used when we request location updates in onConnected
+        /*
+        // This object will be used when we request location updates
         locationRequest = LocationRequest.create()
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
                 .setNumUpdates(1)
@@ -76,62 +59,28 @@ public class MainActivity extends AppCompatActivity
         // give me a way to test location without GPS, so I'm using
         // PRIORITY_HIGH_ACCURCY for now.
         // NumUpdate is one, because I just need to get the location once
-    }
+        */
 
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        googleApiClient.connect();
-    }
-
-
-    @Override
-    public void onStop() {
-        googleApiClient.disconnect();
-        super.onStop();
-    }
-
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
+        locationClient = LocationServices.getFusedLocationProviderClient(this);
         final int MY_PERMISSION_ACCESS_COURSE_LOCATION = 42;  // This is an arbitrary value
         // This check is required by Google Play Services APIs beginning with version 9.0.0 (I think)
-        if( ContextCompat.checkSelfPermission( this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION )
-                != PackageManager.PERMISSION_GRANTED )
-        {
-            // In API 23 and later users must give permission after the activity runs
-            ActivityCompat.requestPermissions( this,
-                    new String[] {  android.Manifest.permission.ACCESS_FINE_LOCATION  },
-                    MY_PERMISSION_ACCESS_COURSE_LOCATION );
-        }
-        else
-        {
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // With API 23 and later, users must give permission after the activity runs
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSION_ACCESS_COURSE_LOCATION);
+        } else {
             // This is where we request the locaiton
-            LocationServices.FusedLocationApi.requestLocationUpdates(
-                    googleApiClient, locationRequest, this);
+            locationClient.getLastLocation();
         }
     }
 
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        currentLocationTextView.setText("Connection failed. Code: " + i);
-    }
-
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        currentLocationTextView.setText("Connection failed. Error code: " +
-                connectionResult.getErrorCode());
-    }
-
-
-    @Override
-    public void onLocationChanged(Location location) {
+  @Override
+    public void onSuccess(Object location) {
         // Here it is! This is where we get the current location
-        myLocation = location;
+        Location myLocation = (Location)location;
         Geocoder geo = new Geocoder(this,
                 Locale.getDefault());
         List<Address> addresses = null;
@@ -144,6 +93,7 @@ public class MainActivity extends AppCompatActivity
         }
 
         currentLocationTextView.setText(addresses.get(0).getLocality());
+
     }
 
     public void calcDistance(View v) {
@@ -159,12 +109,7 @@ public class MainActivity extends AppCompatActivity
             Address address = addresses.get(0);
             double lat = address.getLatitude();
             double lon = address.getLongitude();
-            latLonTextView.setText(Double.toString(lat) + ", " + Double.toString(lon));
-           if( ContextCompat.checkSelfPermission( this,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION )
-                    != PackageManager.PERMISSION_GRANTED ) {
-                myLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-            }
+            latLonTextView.setText(String.format("%s, %s", Double.toString(lat), Double.toString(lon)));
             if (myLocation != null) {
                 Location destinationLocation = new Location("Destination");
                 destinationLocation.setLatitude(lat);
@@ -180,4 +125,5 @@ public class MainActivity extends AppCompatActivity
             distanceTextView.setText("Destination city not found");
         }
     }
+
 }
