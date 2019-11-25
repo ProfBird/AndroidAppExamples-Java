cb9c42fcfae63e14b96c204b6ac64bc358c594ad
= <birdb@lanecc.edu>
2019-11-25 14:50:58 -0800

    New example with SQLite and a CursorAdapter

---
+++ ListViewArrayAdapterParserSQLiteDemo/app/src/main/java/edu/lanecc/birdb/listview_arrayadapter_parser_demo/VocabSqliteHelper.java
@@ -0,0 +1,38 @@
+package edu.lanecc.birdb.listview_arrayadapter_parser_demo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+
+public class VocabSqliteHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "vocab.sqlite";
+    private static final int DATABASE_VERSION = 1;
+    public static final String VOCABULARY = "vocabulary";
+    public static final String SPANISH = "spanish";
+    public static final String ENGLISH = "english";
+    public static final String POS = "pos";
+
+    private Context context = null;
+
+    public VocabSqliteHelper(Context c) {
+        super(c, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = c;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + VOCABULARY
+                + "( _id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + SPANISH + " TEXT,"
+                + ENGLISH + " TEXT,"
+                + POS + " TEXT"
+                + ")" );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // TODO: Manage schema changes
+    }
+}
